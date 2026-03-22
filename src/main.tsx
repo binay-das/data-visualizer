2f0c35ad7f23d4bb78af097f2c9066afad7280cd
Binay <binaydas1123@gmail.com>
2026-03-22 10:40:07 +0530

    wrap App with DatasetProvider

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { ThemeProvider } from './components/theme-provider.tsx'
+import { DatasetProvider } from './hooks/useDatasets.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider defaultTheme="system" storageKey="datascope-theme">
-      <App />
+      <DatasetProvider>
+        <App />
+      </DatasetProvider>
     </ThemeProvider>
   </StrictMode>,
 )
