4f381800460607d0144b0aa4c47ba75870781884
Binay <binaydas1123@gmail.com>
2026-04-01 07:20:30 +0530

    feat(ui): add sonner toast provider and setup to root app

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,14 @@ import './index.css'
 import App from './App.tsx'
 import { ThemeProvider } from './components/theme-provider.tsx'
 import { DatasetProvider } from './hooks/useDatasets.tsx'
+import { Toaster } from "@/components/ui/sonner"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider defaultTheme="system" storageKey="datascope-theme">
       <DatasetProvider>
         <App />
+        <Toaster />
       </DatasetProvider>
     </ThemeProvider>
   </StrictMode>,
